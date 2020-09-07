805e6010bd9e2262885581576a6549410bfb539e
林溪 <z7xisoo@LINXI.local>
2020-09-07 14:57:21 +0800

    feat:  优化背景设置

--- README.md
+++ README.md
@@ -26,36 +26,64 @@ import RainbowWaves from 'rainbow-waves'
 | key|type|default|use|
 | -------- |-----:|:----:|:----:|
 | el|String|"rainbow-waves"| canvas id|
+| new|Boolean|true|create new canvas|
+| clear|Boolean|true|clear canvas when animation|
 | width|Number|1920|canvas width|
 | height|Number|1080|canvas height|
-| backgroundColor|String|"#fff"|canvas background-color|
-| backgroundImage|Object|{...}|canvas background-image|
-| waves|Array|[{...}]|waves|
-
-
-- [x] backgroundImage
-
-| key|type|default|use|range|
-| -------- |-----:|:----:|:----:|:----:|
-| src|String|""|image-url| - |
-| repetition|String|"repeat"|如何重复图像|"repeat","repeat-x","repeat-y","no-repeat"|
-
-```js
-import image from "rainbow-waves.png";
-```
+| background|Object|{...}|canvas background|
 
 
 - [x] waves
 
 | key|type|default|use|range|
 | -------- |-----:|:----:|:----:|:----:|
-| color|String|"blue"|wave color  | - |
 | jitter|Number|0.04|波浪抖动频率|[0.01 - 1.00]|
 | restore|Number|0.03| 波峰差值恢复速度|[0.01 - 1.00]|
 | waveGap|Number|80|波浪峰差| - |
 | waterGap|Number|20|水位差| - |
 | waveUps|Number|6|波浪起伏频率|[1 - 10]|
 | waveHeight|Number|0.45|波浪占比|[0.01 - 1.00]|
+| background|Object|{...}|canvas background|-|
+
+- [x] background
+
+| key|type|default|use|range|
+| -------- |-----:|:----:|:----:|:----:|
+| type|String|""|type|"color","image","gradient"|
+| color|String Array|""|color|-|
+| position|Array|""|image-gradient-positon|[0,0,width,0]|
+| src|String|""|image-url| "color","image","gradient" |
+| repetition|String|""|image-repetition| "color","image","gradient" |
+
+
+```js
+import image from "rainbow-waves.png";
+
+background = {
+  type: "gradient",
+  color: ["red", "orange", "green", "blue"],
+  position:[0,0,1920,1080]
+};
+
+background = {
+  type: "image",
+  src:image,
+  repetition:"repeat", // "repeat","repeat-x","repeat-y","no-repeat"
+};
+
+background = {
+  type: "color",
+  color: ["red", "orange", "green", "blue"],
+};
+
+background = {
+  type: "color",
+  color: "green",
+};
+
+```
+
+
 
 ## LICENSE
 
--- packages/rainbow-waves.vue
+++ packages/rainbow-waves.vue
@@ -1,38 +1,32 @@
 <template>
-  <div class="rainbow-waves">
-    <canvas :id="config.el"></canvas>
+  <div v-if="createCanvas" class="rainbow-waves">
+    <canvas :id="elId"></canvas>
   </div>
 </template>
 
 <script>
+let canvas = null;
+let ctx = null;
+let t = 0;
+let width = 0;
+let height = 0;
+
 export default {
   name: "rainbow-waves",
   props: {
     config: {
       type: Object,
-      default() {
-        return {
-          el: "rainbow-waves",
-          width: 1920,
-          height: 1000,
-          backgroundColor: "#fff",
-          backgroundImage:{
-            src:"",
-            repetition:"",
-          },
-          waves: [
-            {
-              color: "blue",
-              jitter: 0.04,
-              restore: 0.03,
-              waveGap: 80,
-              waterGap: 20,
-              waveUps: 6,
-              waveHeight: 0.45,
-            },
-          ],
-        };
-      },
+    },
+    waves: {
+      type: Array,
+    },
+  },
+  computed: {
+    createCanvas() {
+      return this.config ? this.config.new || true : true;
+    },
+    elId() {
+      return this.config ? this.config.el || "rainbow-waves" : "rainbow-waves";
     },
   },
   watch: {
@@ -43,45 +37,119 @@ export default {
         });
       },
       deep: true,
-      immediate: true,
+    },
+    waves: {
+      handler() {
+        this.$nextTick(() => {
+          this.drow();
+        });
+      },
+      deep: true,
     },
   },
-  mounted() {},
+  mounted() {
+    this.$nextTick(() => {
+      this.drow();
+    });
+  },
   beforeDestroy() {
-    const { el, width, height } = this.config;
-    let canvas = document.getElementById(el);
-    let ctx = canvas.getContext("2d");
-    ctx.rect(0, 0, width, height);
+    if (ctx) ctx.rect(0, 0, width, height);
+    canvas = null;
+    ctx = null;
+    t = 0;
+    width = 0;
+    height = 0;
   },
   methods: {
     drow() {
-      const { el, width, height, waves, backgroundColor,backgroundImage } = this.config;
-      if (waves && waves.length) {
-        let canvas = document.getElementById(el);
-        let ctx = canvas.getContext("2d");
+      // 重置画布
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx = null;
+      }
+
+      // 初始化
+      t = 0;
+      width = 0;
+      height = 0;
+
+      // 配置项
+      const { new: create, el, width, height, clear, background } = {
+        el: "rainbow-waves",
+        clear: true,
+        new: true,
+        width: 1920,
+        height: 1000,
+        background: {
+          type: "color",
+          color: "#fff",
+          // position
+          // src
+          // repetition
+        },
+        ...this.config,
+      };
+
+      // 获取 画布
+      canvas = document.getElementById(el);
+      // 未找到指定元素
+      if (!canvas) throw new Error(`指定ID为 ${el} 的元素未找到`);
+      if (!canvas) return;
+      ctx = canvas.getContext("2d");
+      // 配置 画布
+      if (create) {
         canvas.width = width;
         canvas.height = height;
-        let t = 0;
+        width = width;
+        height = height;
+      }
+      // 颜色配置函数
+      function setColor(background) {
+        // 未找到指定元素
+      if (!ctx) throw new Error(`未找到‘canvas’, getContext 错误`);
+      if (!ctx) return;
+        switch (background.type) {
+          case "color":
+            ctx.fillStyle = Array.isArray(background.color)
+              ? background.color[
+                  Math.ceil(Math.random() * (background.color.length - 1))
+                ]
+              : background.color;
+            break;
+          case "gradient":
+            const gradient = ctx.createLinearGradient(
+              position[0],
+              position[1],
+              position[2],
+              position[3]
+            );
+            color.forEach((x, k) => {
+              gradient.addColorStop(k / color.length, x);
+            });
+            ctx.fillStyle = gradient;
+            break;
+          case "image":
+            let img = new Image();
+            img.src = background.src;
+            ctx.fillStyle = ctx.createPattern(img, background.repetition);
+            break;
+        }
+      }
+
+      // 配置波浪
+      if (this.waves && this.waves.length) {
         loop();
         function loop() {
-          ctx.clearRect(0, 0, width, height);
-          if (backgroundImage && backgroundImage.src) {
-            let repetition = backgroundImage.repetition || "repeat";
-            let img = new Image();
-            img.src = backgroundImage.src;
-            ctx.fillStyle = ctx.createPattern(img, repetition);
-          } else {
-            ctx.fillStyle = backgroundColor;
-          }
+          if (clear) ctx.clearRect(0, 0, width, height);
+          if(create) setColor(background);
           ctx.fillRect(0, 0, width, height);
-          for (let i = 0; i < waves.length; i++) {
-            let color = waves[i]["color"];
-            let { jitter, restore, waveGap, waterGap, waveUps } = waves[i];
+          for (let i = 0; i < this.waves.length; i++) {
+            let { jitter, restore, waveGap, waterGap, waveUps } = this.waves[i];
             ctx.beginPath();
             for (let e = 0; e <= 1 + 0.01; e += 0.01) {
               ctx.lineTo(
                 e * width,
-                height * waves[i]["waveHeight"] +
+                height * this.waves[i]["waveHeight"] +
                   (Math.sin(e * waveUps + t * jitter) * waveGap + waterGap) *
                     Math.sin(t * restore)
               );
@@ -89,7 +157,7 @@ export default {
             ctx.lineTo(width, height);
             ctx.lineTo(0, height);
             ctx.closePath();
-            ctx.fillStyle = color;
+            setColor(this.waves[i]["background"]);
             ctx.fill();
           }
           t++;
